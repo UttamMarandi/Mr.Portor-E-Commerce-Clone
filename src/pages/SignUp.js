46a56f585157df4b2232a859204052b9c7073180
Uttam Marandi <uttammarandi007@gmail.com>
2021-10-16 00:03:35 +0530

    password show hide funcitonality

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Header from "../components/Header";
+import { EyeIcon } from "@heroicons/react/outline";
+import { EyeOffIcon } from "@heroicons/react/outline";
 
 const SignUp = () => {
+  const [caretColor, setCaretColor] = useState("black");
+  const inputEl = useRef(null);
+  const [values, setValues] = useState({
+    password: "",
+    showPassword: false,
+  });
+  const handleClickShowPassword = () => {
+    setCaretColor("transparent");
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  };
+  const handlePasswordChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
   return (
     <div className="signup_page">
       <Header />
@@ -9,7 +27,7 @@ const SignUp = () => {
         <div className="signup_form pt-16">
           <h2 className="text-3xl">Register</h2>
           <form action="">
-            <div className="name pt-8 block">
+            <div className="name pt-8 block focus:bg-red-300">
               <label htmlFor="" className="label ">
                 Email Address
               </label>
@@ -26,6 +44,30 @@ const SignUp = () => {
                 className="input block border border-gray-400 py-3 px-3 w-full focus:outline-none "
                 type="password"
               />
+            </div>
+            <div className="password_2 block">
+              <label>Enter your password</label>
+              <div className="eye_div">
+                <input
+                  className="input block border border-gray-400 py-3 px-3 w-full focus:outline-none "
+                  href={inputEl}
+                  style={{ caretColor: caretColor }}
+                  type={values.showPassword ? "text" : "password"}
+                  onChange={handlePasswordChange("password")}
+                  value={values.password}
+                />
+                <div
+                  className="icon_button"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? (
+                    <EyeIcon className="h-6 font-extralight" />
+                  ) : (
+                    <EyeOffIcon className="h-6 font-extralight" />
+                  )}
+                </div>
+              </div>
             </div>
           </form>
         </div>
